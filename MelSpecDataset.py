ae1779a6241eb23555cdcd88695d370b78681af8
SeungminHeo <hawi75@yonsei.ac.kr>
2020-07-10 06:03:50 +0900

    feat: pytorch dataset

--- MelSpecDataset.py
+++ MelSpecDataset.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import pickle
 
 import torch
 from torch.utils.data import Dataset
@@ -9,23 +10,23 @@ import numpy as np
 class MelSpecDataset(Dataset):
     def __init__(
             self,
-            data_dir: Path,
-            meta_file_path: Path,
+            data_path: Path,
+            target_json_path: Path,
             ):
-        self.data_dir = data_dir
-        with open(f'{meta_file_path}/song_meta.json', 'r', encoding='utf-8') as fb:
-            self.song_meta = json.load(fb)
+        self.data_dir = data_path
+        with open(data_path, "rb") as fp:
+            self.data_files = pickle.load(fp)
+        with open(target_json_path, 'r', encoding='utf-8') as fb:
+            self.target_dict = json.load(fb)
 
     def __getitem__(self, item_index: int):
-        inputs = torch.FloatTensor(np.load(f"{self.data_dir}/{self.get_song_id(item_index)}.npy"))
-        targets = torch.FloatTensor([self.get_artist_id(item_index)])
+        audio_data = np.load(self.data_files[item_index])
+        inputs = torch.from_numpy(audio_data)
+        targets = torch.from_numpy(np.array(self.get_target(item_index))).to(dtype=torch.long)
         return inputs, targets
 
     def __len__(self):
-        return len(self.song_meta)
+        return len(self.data_files)
 
-    def get_artist_id(self, item_index: int):
-        return self.song_meta[item_index]["artist_id"]
-
-    def get_song_id(self, item_index: int):
-        return int(self.song_meta[item_index]["id"])
+    def get_target(self, item_index: int):
+        return self.target_dict[self.data_files[item_index].split("/")[-1].split(".")[0]]
--- SpeakerCLSModel.py
+++ SpeakerCLSModel.py
@@ -1,10 +1,14 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from pytorch_lightning import LightningModule
+from MelSpecDataset import MelSpecDataset
 
 
-class STModel(nn.Module):
+class SpeakerCLSModel(LightningModule):
     def __init__(self):
-        super(STModel, self).__init__()
+        super().__init__()
         self.conv_layer = nn.Sequential(
             # Conv2d_0
             nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(1, 1), padding=[0, 0]),
@@ -28,7 +32,7 @@ class STModel(nn.Module):
             nn.Dropout(p=0.1)
         )
 
-        self.lstm_layer = nn.LSTM(128, 64, num_layers=2)
+        self.lstm_layer = nn.LSTM(128, 64, num_layers=2, bidirectional=True)
 
         self.fc_layer = nn.Sequential(
             nn.Dropout(p=0.3),
@@ -36,10 +40,40 @@ class STModel(nn.Module):
             nn.Linear(in_features=64, out_features=10, bias=True)
         )
 
-
     def forward(self, x):
         out = self.conv_layer(x)
         out = self.lstm_layer(x)
         out = out.view(batch_size, -1)
         out = self.fc_layer(out)
         return out
+
+    def forward(self, x):
+        return F.relu(self.l1(x.view(x.size(0), -1)))
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        return {'loss': F.cross_entropy(y_hat, y)}
+
+
+    def train_dataloader(self, data_dir, meta_file_path):
+        data_loader = DataLoader(
+            dataset=MelSpecDataset(
+                data_dir=data_dir,
+                meta_file_path=meta_file_path
+            ),
+            batch_size=16
+        )
+        return data_loader
+
+    def valid_dataloader(self, data_dir, meta_file_path):
+        data_loader = DataLoader(
+            dataset=MelSpecDataset(
+                data_dir=data_dir,
+                meta_file_path=meta_file_path
+            )
+        )
+        return data_loader
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.02)
