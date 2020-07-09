c7ef01a43cd1c79d9d640f1e677104bf5fe62ea2
SeungminHeo <hawi75@yonsei.ac.kr>
2020-07-10 06:04:48 +0900

    feat: add ckpt callback and profiler

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from MelSpecDataset import MelSpecDataset
-
 import torch
 from torch.utils.data import DataLoader
-
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from SpeakerCLSModel import SpeakerCLSModel
+from pytorch_lightning.profiler import AdvancedProfiler
 
 if __name__ == '__main__':
+    checkpoint_callback = ModelCheckpoint(filepath='/datadrive/checkpoint/{epoch}-{val_loss:.2f}-{val_accuracy:.2f}.ckpt')
+    model = SpeakerCLSModel(train_data_dir="/datadrive/train.txt",
+                            valid_data_dir="/datadrive/val.txt",
+                            target_json_dir="/datadrive/target.json",
+                            output_size=7296,
+                            batch_size=128)
+    trainer = Trainer(gpus=1,
+                      max_epochs=10,
+                      limit_val_batches=1.0,
+                      check_val_every_n_epoch=1,
+                      checkpoint_callback=checkpoint_callback,
+                      distributed_backend="dp",
+                      profiler=AdvancedProfiler(),)
+    trainer.fit(model)
 
-    data_loader = DataLoader(
-        dataset=MelSpecDataset(
-            data_dir="/home/seungmin/ybigta-conf/data",
-            meta_file_path="/home/seungmin/yㅌㅈbigta-conf"
-        ),
-        batch_size=16
-    )
-
-    for data in data_loader:
-        print(data[0].shape)
-        print(data[1].shape)
